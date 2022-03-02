b87a9e2e54ff4f23ce4029fcee087002638da2dd
bishadcs <binshadcs@gmail.com>
2022-03-02 15:24:45 +0530

    tesing changes

--- base/models.py
+++ base/models.py
@@ -3,7 +3,8 @@ from tkinter.tix import Tree
 from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+# Create your models here. this is testing chenge
+
 class Task(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True)
