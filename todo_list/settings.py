f33ed1a2721023e7d6f70b0bebbbadf522c98cce
bishadcs <binshadcs@gmail.com>
2022-03-28 22:21:59 +0530

    requrment edited

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,7 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR)
 STATIC_URL = '/static/'
 
 # Default primary key field type
